b3ae8a29aa18a10c0f3513d92a61fdabe9c1a934
Lenovo@DESKTOP-1A9ET6S <haikalfebriansyah743@gmail.com>
2025-04-27 18:52:40 +0700

    FIX IP

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const errorHandler = require('./middlewares/errorHandler');
 
 // Konfigurasi CORS
 app.use(cors({
-  origin: 'http://54.253.189.2', // Ganti dengan URL frontend kamu (bisa `*` untuk semua origin)
+  origin: 'http://13.236.193.132/', // Ganti dengan URL frontend kamu (bisa `*` untuk semua origin)
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Sesuaikan dengan metode yang diizinkan
   allowedHeaders: ['Content-Type', 'Authorization'], // Tambahkan headers yang dibutuhkan
 }));
